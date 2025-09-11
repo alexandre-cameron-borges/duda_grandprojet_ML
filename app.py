0e3ee66e09ab17ee5089be2557c5683df8b860f9
alexandre-cameron-borges <alexandre@ppcetoile.com>
2025-09-11 18:43:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from predict import make_prediction
 
 # --- Configuration de la page ---
 st.set_page_config(
-    page_title="Prédiction de Churn Client",
+    page_title="Prédiction du Churn Client Ecommerce SDUDA25",
     page_icon="🔮",
     layout="wide"
 )
 
 # --- Interface principale ---
-st.title("Prédiction du Churn Client 🔮")
+st.title("Prédiction du Churn Client Ecommerce SDUDA25 🔮")
 st.write("""
-Cette application prédit la probabilité qu'un client résilie son abonnement (churn).
+Cette application de Alexandre, Ryad & Jordan prédit la probabilité qu'un client résilie son abonnement (churn).
 Ajustez les caractéristiques du client ci-dessous et cliquez sur "Lancer la Prédiction" pour voir le résultat.
 """)
 st.markdown("---")
